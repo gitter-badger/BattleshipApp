d42303e61f916d6048bc98cc49eeda07384aae2d
Katye Russell <katye.russell@gmail.com>
2015-02-27 15:08:55 -0600

    Some random code.  Trying to correct my branch to match Margaret's code.

--- app/scripts/index.js
+++ app/scripts/index.js
@@ -1,27 +1,47 @@
 'use strict';
 
-var fb = new Firebase('https://xogame.firebaseio.com/'),
-    fbusers = new Firebase('https://xogame.firebaseio.com/users'),
-    fbgames = new Firebase('https://xogame.firebaseio.com/games');
+var fb = new Firebase('https://battleship16.firebaseio.com/');
+var b;
 
-
-var currentGame,userRaw,userIndex,userShort;
-
-//if user authenticated, find username
-if(fb.getAuth()){
-  userRaw = fb.getAuth().password.email;
-  userIndex = userRaw.indexOf('@');
-  userShort = userRaw.substr(0,userIndex);
-} else {}
-
-  /////push game data to fb
-  function createGame(user){
-    var newGameData = {'user1': user, 'user2': '', 'gameboard': [0, 0, 0, 0, 0, 0, 0, 0, 0], 'gameover': false, 'symbol': 'X'};
-    fbgames.push(newGameData);
-    findGameId();
-    return newGameData;
-  }
-   
-  $('form').submit(function(){
-  fb.child('/Games').push(({'board': board, 'player1': false, 'player2': false, 'turn': ''}));
+$('button').on('click', function(){
+  createBoard();
 })
+
+function createBoard (b) {
+  b = [['','','','',''],['','','','',''],['','','','',''],['','','','',''],['','','','','']];
+  var $table = $('<table></table>');
+  b.forEach(function (row) {
+    var $tr = $('<tr></tr>');
+    row.forEach(function (cell){
+      $tr.append($('<td></td>'));
+      $table.append($tr);
+    });
+  });
+  $('.board').append($table);
+}
+
+
+
+
+
+// var currentGame,userRaw,userIndex,userShort;
+
+// //if user authenticated, find username
+// if(fb.getAuth()){
+//   userRaw = fb.getAuth().password.email;
+//   userIndex = userRaw.indexOf('@');
+//   userShort = userRaw.substr(0,userIndex);
+// } else {}
+
+//   /////push game data to fb
+//   function createGame(user){
+//     var newGameData = {'user1': user, 'user2': '', 'gameboard': [0, 0, 0, 0, 0, 0, 0, 0, 0], 'gameover': false, 'symbol': 'X'};
+//     fbgames.push(newGameData);
+//     findGameId();
+//     return newGameData;
+//   }
+   
+//   $('form').submit(function(){
+//   fb.child('/Games').push(({'board': board, 'player1': false, 'player2': false, 'turn': ''}));
+// })
+
