b713c6fba084281327ff02b4837192453c916378
Andy Meagher <ameagher311@gmail.com>
2015-02-27 14:47:50 -0600

    trying to get margarets js

--- app/scripts/index.js
+++
@@ -1,4 +0,0 @@
-$('.newgame').on('click', function(){
-	var tr = '<tr><td></td><td></td><td></td><td></td><td></td></tr>';
-	$('.container').append($('<table class="board"><tbody>' + tr + tr + tr + tr + tr + '</tbody></table>'));
-});
